5ab9c70f2c3aa08e31ccb05867ec4739b41d22dc
Test User <test@example.com>
2026-02-06 08:35:02 +0530

    code push

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Blue-Green App v1");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
